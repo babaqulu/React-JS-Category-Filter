e3f148d1d69b9c93c7f944c7dbe7b0000d4f6366
babaqulu <baba.qulu97@gmail.com>
2023-08-29 18:36:58 +0400

    updated”

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Filter from "./test/filter";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <Filter />
+
+    </div>
+  );
+}
+
+export default App;
